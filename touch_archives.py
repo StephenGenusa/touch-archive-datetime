f3797cdedb68e67e8a32b3cd22d0af628b7d6108
Stephen Genusa <github@genusa.com>
2015-03-01 08:59:15 -0600

    Added Ruby .gem support

--- touch_archives.py
+++ touch_archives.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import os
 import sys
 import time
@@ -5,11 +7,12 @@ import datetime
 import tarfile
 import gzip
 import zipfile
+import re
+import dateutil.parser
 
+only_test_validity_of_archive = False
 
-###############################################################
-# switch class from http://code.activestate.com/recipes/410692/
-###############################################################
+# From http://code.activestate.com/recipes/410692/
 class switch(object):
     def __init__(self, value):
         self.value = value
@@ -29,9 +32,8 @@ class switch(object):
             return True
         else:
             return False
-###############################################################
 
-
+# From http://stackoverflow.com/questions/16976192/whats-the-way-to-extract-file-extension-from-file-name-in-python
 def splitext(path):
     for ext in ['.tar.gz', '.tar.bz2']:
         if path.endswith(ext):
@@ -41,66 +43,97 @@ def splitext(path):
 
 def touch_file(file_name, mod_time):
     try:
-        if mod_time > 0 and mod_time < 1609376461:
-            os.utime(file_name, ((mod_time,mod_time)))
+        if not only_test_validity_of_archive :
+            if mod_time > 0 and mod_time < 1609376461:
+                os.utime(file_name, ((mod_time,mod_time)))
+                print "  Touched  " + file_name
+            else:
+                print "  Skipped  " + file_name
     except:
         pass
 
+
+def log_error(message):
+    open("file_errors.txt", 'a').writelines(message + '\n')
+    
 
 def get_time_for_tarfile(file_name):
     newest_time = 0
     try:
         with tarfile.TarFile.open(file_name, 'r') as tarredFile:
             members = tarredFile.getmembers()
-            for member in members:
-                if member.mtime > newest_time:
-                    newest_time = member.mtime
+            if not only_test_validity_of_archive :
+                for member in members:
+                    if member.mtime > newest_time:
+                        newest_time = member.mtime
     except:
-        open("file_errors.txt", 'a').writelines(file_name+'\n')
+        log_error(file_name + ": " + str(sys.exc_info()[0]))
     return newest_time
+    
     
 def get_time_for_zipfile(file_name):
     newest_time = 0
     try:
         with zipfile.ZipFile(file_name, 'r') as zippedFile:
             members = zippedFile.infolist()
-            for member in members:
-                #print member.orig_filename            
-                curDT = time.mktime(datetime.datetime(*member.date_time).timetuple())
-                if curDT > newest_time:
-                    newest_time = curDT
+            if not only_test_validity_of_archive :
+                for member in members:
+                    curDT = time.mktime(datetime.datetime(*member.date_time).timetuple())
+                    if curDT > newest_time:
+                        newest_time = curDT
     except:
-        open("file_errors.txt", 'a').writelines(file_name+'\n')
+        log_error(file_name + ": " + str(sys.exc_info()[0]))
     return newest_time
 
+def touch_gem_file(file_name):
+    with tarfile.TarFile.open(file_name, 'r') as tarredFile:
+        members = tarredFile.getmembers()
+        for member in members:
+            if member.name == 'metadata.gz':
+                try:
+                    tarredFile.extract(member, "/tmp")
+                    content = gzip.open('/tmp/metadata.gz','rb')
+                    metadata = content.read()
+                    os.remove('/tmp/metadata.gz')
+                    match = re.search("date:(.{0,50}?)\n", metadata, re.DOTALL | re.MULTILINE)
+                    if match:
+                        parsed_datetime = match.groups(0)[0]
+                    else:
+                        parsed_datetime = ""
+                    dt = dateutil.parser.parse(parsed_datetime)
+                    touch_file(file_name, time.mktime(dt.timetuple()))
+                except:
+                    log_error(file_name + ": " + str(sys.exc_info()[0]))
+    
 
-def process_file(filename_to_process, verbose):
+def process_file(filename_to_process):
+    filename, file_extension = splitext(filename_to_process)
     if os.path.getsize(filename_to_process) > 0:
-        filename, file_extension = splitext(filename_to_process)
         for case in switch(file_extension.lower()):
             if case('.zip'):
-                if verbose: print "    " + filename_to_process
+                touch_file(filename_to_process, get_time_for_zipfile(filename_to_process))
+                break
+            if case('.whl'):
                 touch_file(filename_to_process, get_time_for_zipfile(filename_to_process))
                 break
             if case('.egg'):
-                if verbose: print "    " + filename_to_process
                 touch_file(filename_to_process, get_time_for_zipfile(filename_to_process))
                 break
             if case('.tar'):
-                if verbose: print "    " + filename_to_process
                 touch_file(filename_to_process, get_time_for_tarfile(filename_to_process))
                 break
             if case('.tar.gz'):
-                if verbose: print "    " + filename_to_process
                 touch_file(filename_to_process, get_time_for_tarfile(filename_to_process))
                 break
             if case('.tar.bz2'):
-                if verbose: print "    " + filename_to_process
                 touch_file(filename_to_process, get_time_for_tarfile(filename_to_process))
                 break
-            # The reason the following cases are included but ignored is so that unhandled file
-            # extensions which occur are printed with the intention of, if possible, 
-            # adding support for that file type.
+            if case('.tgz'):
+                touch_file(filename_to_process, get_time_for_tarfile(filename_to_process))
+                break
+            if case('.gem'):
+                touch_gem_file(filename_to_process)
+                break
             if case(''):
                 break
             if case('.html'):
@@ -116,19 +149,19 @@ def process_file(filename_to_process, verbose):
             if case(''): # default, could also just omit condition or 'if True'
                 print "Extension '" + file_extension.lower() + "' not handled."
     else:
-        open("file_errors.txt", 'a').writelines('(empty file) ' + filename_to_process + '\n')
-        
+        log_error('Empty file: ' + filename_to_process)
+
 
 def main(root_path):
     for root, dirs, files in os.walk(root_path):
         print 'Processing Path ' + root
         for file in files:
-            process_file(root + "/" + file, False)
+            process_file(root + "/" + file)
     print "\nStephen's Archive Re-Touch Utility Complete\n"
 
 
 ##########################################
-# Change this later to a commandline param
+# Change this later to a command-line param
 ##########################################
 if __name__ == "__main__":
-    main(root_path='/Volumes/Python')
+    main(root_path='/data/rubygems/gems')
